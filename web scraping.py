1f97323fb4a9a35506ad031f57f7116a2a0f4204
Akhil Kumar <akhip1903@gmail.com>
2025-09-25 09:12:40 +0530

    web scraping.py

--- web scraping.py
+++ web scraping.py
@@ -8,10 +8,11 @@ response.raise_for_status()
 soup = BeautifulSoup(response.text, "html.parser")
 headlines = soup.find_all("h2")
 
-with open("C:/Users/akhip/headlines.txt", "w", encoding="utf-8") as f:
+with open("headlines.txt", "w", encoding="utf-8") as f:
     f.write("📰 Latest News Headlines\n\n")
     for i, h in enumerate(headlines[:10], 1):
         title = h.get_text(strip=True)
         f.write(f"{i}. {title}\n")
+
 
 print("✅ Headlines saved to headlines.txt")
